01c958911a530ea23f2438fe3cf10f48e99b13c1
duanfa <duanfa1987@gmail.com>
2018-01-02 17:37:08 +0800

    kmeans

--- tf/helloworld/chap2/K-Means.py
+++ tf/helloworld/chap2/K-Means.py
@@ -19,24 +19,28 @@ for i in xrange(num_vectors):
     
 df = pd.DataFrame({"x": [v[0] for v in vector_values],
                    "y": [v[1] for v in vector_values]})
+
 sns.lmplot("x", "y", data=df, fit_reg=False, size=7)
-plt.show()
+# plt.show()
 
 vectors = tf.constant(vector_values)
-centroids = tf.Variable(tf.slice(tf.random_shuffle(vectors),
-                                 [0, 0], [num_clusters, -1]))
+print ("vectors shape:%s" %vectors.get_shape())
+centroids = tf.Variable(tf.slice(tf.random_shuffle(vectors),[0, 0], [num_clusters, -1]))
 
 
 expanded_vectors = tf.expand_dims(vectors, 0)
 expanded_centroids = tf.expand_dims(centroids, 1)
 
-print (expanded_vectors.get_shape())
-print (expanded_centroids.get_shape())
+print ("expanded_vectors shape:%s" %expanded_vectors.get_shape())
+print ("expanded_centroids shape:%s" %expanded_centroids.get_shape())
 
+subtract = tf.subtract(expanded_vectors, expanded_centroids)
+print ("subtract shape:%s" %subtract.get_shape())
 
-distances = tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroids)), 2)
+distances = tf.reduce_sum(tf.square(subtract), 2)
+print ("distances shape:%s" %distances.get_shape())
 assignments = tf.argmin(distances, 0)
-
+print ("assignments shape:%s" %assignments.get_shape())
 
 means = tf.concat([tf.reduce_mean(
       tf.gather(vectors,
@@ -57,8 +61,10 @@ with tf.Session() as sess:
     _, centroid_values, assignment_values = sess.run([update_centroids,
                                                       centroids,
                                                       assignments])
+
   print ("centroids")
   print (centroid_values)
+  print (assignment_values)
   
 data = {"x": [], "y": [], "cluster": []}
 for i in xrange(len(assignment_values)):
@@ -69,4 +75,4 @@ df = pd.DataFrame(data)
 sns.lmplot("x", "y", data=df,
            fit_reg=False, size=7,
            hue="cluster", legend=False)
-plt.show()
+# plt.show()
